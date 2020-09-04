263534b4c495101465251dc6476b4af0f1f722ca
KevinVel <kevinestuardovd@gmail.com>
2020-09-04 14:25:55 -0600

    Interfaz de Usuario

---
+++ Panel/inserciones.php
@@ -0,0 +1,62 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+$con   = mysqli_connect('localhost','root','','mydb');// 
+//$con   = mysqli_connect('server','user','pass','bd');// Check
+if ($con->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "NO CONECTA";
+}
+if($_POST["insertaProducto"] == "nuevoProducto"){
+
+        $imgFile1     = $_FILES['imagen1']['name'];
+        $tmp_dir1     = $_FILES['imagen1']['tmp_name'];
+        $imgSize1     = $_FILES['imagen1']['size'];
+
+        $imgFile2     = $_FILES['imagen2']['name'];
+        $tmp_dir2     = $_FILES['imagen2']['tmp_name'];
+        $imgSize2     = $_FILES['imagen2']['size'];
+
+        $imgFile3     = $_FILES['imagen3']['name'];
+        $tmp_dir3     = $_FILES['imagen3']['tmp_name'];
+        $imgSize3      = $_FILES['imagen3']['size'];
+        
+        $upload_dir1   = 'imagenesProductos/'; // upload directory
+        $upload_dir2   = 'imagenesProductos/'; // upload directory
+        $upload_dir3   = 'imagenesProductos/'; // upload directory
+        $imgExt1      = strtolower(pathinfo($imgFile1,PATHINFO_EXTENSION)); // get image extension
+        $imgExt2      = strtolower(pathinfo($imgFile2,PATHINFO_EXTENSION)); // get image extension
+        $imgExt3      = strtolower(pathinfo($imgFile3,PATHINFO_EXTENSION)); // get image extension
+    
+        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+        $imgProducto1     = rand(1000,1000000).".".$imgExt1;
+        $imgProducto2     = rand(1000,1000000).".".$imgExt2;
+        $imgProducto3     = rand(1000,1000000).".".$imgExt3;
+        
+        $tmp_dirUp1   = $_FILES['imagen1']['tmp_name'];
+        $tmp_dirUp2   = $_FILES['imagen2']['tmp_name'];
+        $tmp_dirUp3   = $_FILES['imagen3']['tmp_name'];
+        move_uploaded_file($tmp_dirUp1,$upload_dir1.$imgProducto1);
+        move_uploaded_file($tmp_dirUp2,$upload_dir2.$imgProducto2);
+        move_uploaded_file($tmp_dirUp3,$upload_dir3.$imgProducto3);
+                
+        $insertaProducto = "insert INTO `tbl_encabezado_producto`  
+                VALUES ('','".$_POST["codigoProducto"]."','".$_POST["nombre"]."','".$_POST["descripcion"]."','".$_POST["estado"]."')";
+        $productoInsertado   = $con->query($insertaProducto);
+        $llavePrimaria       = mysqli_insert_id($con);
+
+        $insertaDetalle = "insert INTO `tbl_detalle_producto`  
+                            VALUES ('','".$_POST["precio"]."','".$_POST["precioOferta"]."','".$_POST["marca"]."','".$_POST["imagen1"]."','".$_POST["imagen2"]."','".$_POST["imagen3"]."','".$llavePrimaria."','".$_POST["idCategoria"]."')";
+        $detalleInsertado   = $con->query($insertaDetalle);
+        echo "<script language='javascript'>alert('Producto almacenado exitosamente');</script>";
+        echo "<script language='javascript'>window.open('integradoProductos.php','_self',);</script>";
+    }
+
+ if($_POST["insertaCategoria"] == "nuevaCategoria"){
+    echo "Categoria insertada exitosamente";
+    $insertaCategoria  = "insert INTO `tbl_categorias`  
+                          VALUES ('','".$_POST["nombre"]."','".$_POST["descripcion"]."','".$_POST["estado"]."')";
+    $categoriaInsertada = $con->query($insertaCategoria);
+     echo "<script language='javascript'>alert('Categoría creada exitosamente');</script>";
+     echo "<script language='javascript'>window.open('listadoProductos.php','_self',);</script>";
+    }
+?>
---
+++ Panel/nuevaCategoria.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Muebles | Velasquez</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+  <?php 
+    include 'menu.php';
+    ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Nueva Categoría</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item active">                      
+                <a href="listadoProductos.php"><button type="button" class="btn btn-block btn-secondary">Ver listado producto</button></a>
+              </li>
+              <li class="">                      
+                <a href="nuevoProducto.php"><button type="button" class="btn btn-block btn-warning">Nuevo producto</button></a>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <!-- general form elements disabled -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Datos de la categoría</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <form method="POST" role="form" action="inserciones.php">
+                  <div class="row">
+                  <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Nombre categoría</label>
+                        <input name="nombre" id="nombre" type="text" class="form-control" placeholder="Ingrese nombre" required="">
+                        <input  hidden name="insertaCategoria" id="insertaCategoria" value="nuevaCategoria" type="text" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <!-- textarea -->
+                      <div class="form-group">
+                        <label>Descripción</label>
+                        <textarea name="descripcion" id="descripcion" class="form-control" rows="4" placeholder="Ingrese descripción" ></textarea>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Seleccione estado</label>
+                        <select class="form-control" name="estado" id="estado">
+                          <option>Activo</option>
+                          <option>Inactivo</option>
+                        </select>
+                      </div>
+                     </div>
+                     <div class="form-group">
+                       <label><font color="red">Revisa antes de CREAR CATEGORIA que todos los datos sean correctos!</font></label>
+                     </div>
+                   <center>
+                    <div class="col-sm-12">
+                     <div class="form-group">
+                       <button type="submit" class="btn btn-block bg-gradient-success btn-lg">Crear categoria</button>
+                </div>
+                  </div>
+                </form>
+              </div>
+           </div>
+      </div><!-- /.container-fluid -->
+    </div>
+  </div>
+    </section>
+    <!-- /.content -->
+    <!-- /.content -->
+  </div>
+
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.5
+    </div>
+  </footer>
+
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true,
+      "autoWidth": false,
+    });
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>
